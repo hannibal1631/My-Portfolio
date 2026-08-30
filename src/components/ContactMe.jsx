0b1808bb11d60f9c42a0074e06abc6c1427aea53
hannibal1631 <mukherjeerupak860@gmail.com>
2026-08-30 13:33:20 +0530

    add major UI changes

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,10 +1,4 @@
-import React from 'react';
-import {
-  FaLinkedin,
-  FaEnvelope,
-  FaPhone,
-  FaGithubSquare,
-} from 'react-icons/fa';
+import { FaLinkedin, FaEnvelope, FaGithubSquare } from 'react-icons/fa';
 import { FaXTwitter, FaDiscord } from 'react-icons/fa6';
 import { SiFrontendmentor } from 'react-icons/si';
 import pipboy from './logo/pipboy.png';
@@ -13,22 +7,22 @@ export default function ContactMe() {
   return (
     <div
       id='contact'
-      className='bg-[#1a2b1e] flex flex-col md:flex-row items-center min-h-80 gap-8 p-6 text-green-500'
+      className='bg-[#1a2b1e] w-full flex flex-col md:flex-row items-center gap-8 p-6 text-green-500'
     >
       {/* Image Section */}
-      <div className='w-full md:w-1/3 flex flex-col justify-center items-center'>
-        <img src={pipboy} alt='Contact' className='w-40 h-40' />
-        <h2 className='text-2xl md:text-3xl font-bold uppercase text-green-400 text-center my-4 md:my-6'>
+      <div className='w-full md:w-1/2 flex flex-col justify-center items-center'>
+        <img src={pipboy} alt='Contact' className='w-40 h-40 md:w-70 md:h-70' />
+        <h2 className='text-2xl md:text-3xl font-bold uppercase text-lime-400 text-center my-4 md:my-6'>
           Contact Me
         </h2>
       </div>
 
       {/* Contact Grid */}
-      <div className='w-full md:w-2/3 grid grid-cols-1 sm:grid-cols-2 gap-4'>
+      <div className='w-full md:w-1/2 grid place-items-center grid-cols-2 gap-4 md:gap-x-0'>
         <ContactCard
           icon={<FaLinkedin />}
           label='LinkedIn'
-          link='https://www.linkedin.com/in/rupak-mukherjee-725ba322b/'
+          link='https://www.linkedin.com/in/rupak-mukherjee-fed/'
         />
         <ContactCard
           icon={<FaXTwitter />}
@@ -64,28 +58,15 @@ export default function ContactMe() {
 }
 
 function ContactCard({ icon, label, link }) {
-  let hoverText = label; // Default hover text
-
-  if (link.startsWith('mailto:')) {
-    hoverText = 'Send an Email';
-  } else if (link.startsWith('tel:')) {
-    hoverText = 'Call Now';
-  } else if (link.startsWith('https://')) {
-    hoverText = 'Visit Profile';
-  }
-
   return (
     <a
       href={link}
-      className='flex items-center gap-4 p-4 rounded-md bg-green-900 bg-opacity-20 border border-green-500 transition duration-300 hover:bg-[#1a2b1e] hover:bg-opacity-30 relative'
+      className='flex flex-col items-center justify-center p-4 w-20 h-20 lg:w-30 lg:h-30 gap-4 border border-lime-400/65 transition-all duration-200 opacity-75 hover:opacity-100 hover:border-lime-400 hover:bg-lime-400/10 hover:underline hover:decoration-dashed cursor-pointer'
       target='_blank'
       rel='noopener noreferrer'
     >
-      <span className='text-2xl'>{icon}</span>
-      <span className='text-lg'>{label}</span>
-      <span className='absolute inset-0 flex items-center justify-center rounded-md opacity-0 transition-opacity duration-300 hover:opacity-100 bg-[#1a2b1e]/50 bg-opacity-40 backdrop-blur-md text-green-500 text-lg p-2'>
-        {hoverText}
-      </span>
+      <span className='text-3xl md:text-5xl text-lime-400'>{icon}</span>
+      <span className='hidden md:block md:text-sm line-clamp-1 text-lime-400'>{label}</span>
     </a>
   );
 }
