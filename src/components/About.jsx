e5cf8f07a0f901816c8ad6a05672e42642f2a307
hannibal1631 <mukherjeerupak860@gmail.com>
2026-08-30 09:49:37 +0530

    add UI changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,27 +14,27 @@ function About() {
           src={aboutMe}
           alt='Your Image'
         />
-        <h2 className='text-2xl md:text-3xl font-bold uppercase text-green-400 text-center my-4 md:my-6'>
+        <h2 className='text-2xl md:text-3xl font-bold uppercase text-lime-400 text-center my-4 md:my-6 tracking-wide'>
           About Me
         </h2>
       </div>
 
       {/* Right Section: About Me Text */}
       <div className='text-center lg:text-left max-w-2xl'>
-        <p className='text-green-600 text-lg lg:text-2xl font-semibold'>
+        <p className='text-lime-400 text-lg lg:text-2xl'>
           I’m a self-taught
-          <span className='text-green-400'> Front-End Developer</span> with a
+          <span className='text-amber-500 font-semibold'> Front-End Developer</span> with a
           deep passion for crafting seamless, responsive web experiences. What
           drives me is the challenge of turning creative designs into
           interactive, pixel-perfect websites that work flawlessly across all
-          devices. From mastering <span className='text-green-400'>HTML</span>,{' '}
-          <span className='text-green-400'>CSS</span>, and{' '}
-          <span className='text-green-400'>JavaScript</span> to exploring the
-          latest in <span className='text-green-400'>React.js</span> and{' '}
-          <span className='text-green-400'>Tailwind CSS</span>, I’m always
+          devices. From mastering <span className='text-amber-500 font-semibold'>HTML</span>,{' '}
+          <span className='text-amber-500 font-semibold'>CSS</span>, and{' '}
+          <span className='text-amber-500 font-semibold'>JavaScript</span> to exploring the
+          latest in <span className='text-amber-500 font-semibold'>React.js</span> and{' '}
+          <span className='text-amber-500 font-semibold'>Tailwind CSS</span>, I’m always
           pushing myself to grow.
         </p>
-        <p className='text-green-600 text-lg lg:text-2xl font-semibold mt-4'>
+        <p className='text-lime-400 text-lg lg:text-2xl mt-4'>
           Though I don’t have a formal degree in computer science, my journey
           has been fueled by curiosity, hands-on learning, and the relentless
           pursuit of improvement. I’m always excited to take on new challenges
